f5a745aa3431d1461980323737d728efd25b5265
Enzo1s <enziitoomartin@gmail.com>
2021-04-16 20:23:27 -0300

    Entidades Artiulos, corrección error tipeo

---
+++ BuenSabor/src/main/java/com/delivery/BuenSabor/ArticuloInsumo/entity/ArticuloInsumo.java
@@ -0,0 +1,128 @@
+package com.delivery.BuenSabor.ArticuloInsumo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "articulo_insumo")
+public class ArticuloInsumo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String denominacion;
+	
+	@Column(name = "precio_compra")
+	private double precioCompra;
+	
+	@Column(name = "precio_venta")
+	private double precioVenta;
+	
+	@Column(name = "stock_actual")
+	private double stockActual;
+	
+	@Column(name = "stock_minimo")
+	private double stockMinimo;
+	
+	@Column(name = "unidad_medida")
+	private String unidadMedida;
+	
+	@Column(name = "es_insumo")
+	private boolean esInsumo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDenominacion() {
+		return denominacion;
+	}
+
+	public void setDenominacion(String denominacion) {
+		this.denominacion = denominacion;
+	}
+
+	public double getPrecioCompra() {
+		return precioCompra;
+	}
+
+	public void setPrecioCompra(double precioCompra) {
+		this.precioCompra = precioCompra;
+	}
+
+	public double getPrecioVenta() {
+		return precioVenta;
+	}
+
+	public void setPrecioVenta(double precioVenta) {
+		this.precioVenta = precioVenta;
+	}
+
+	public double getStockActual() {
+		return stockActual;
+	}
+
+	public void setStockActual(double stockActual) {
+		this.stockActual = stockActual;
+	}
+
+	public double getStockMinimo() {
+		return stockMinimo;
+	}
+
+	public void setStockMinimo(double stockMinimo) {
+		this.stockMinimo = stockMinimo;
+	}
+
+	public String getUnidadMedida() {
+		return unidadMedida;
+	}
+
+	public void setUnidadMedida(String unidadMedida) {
+		this.unidadMedida = unidadMedida;
+	}
+
+	public boolean isEsInsumo() {
+		return esInsumo;
+	}
+
+	public void setEsInsumo(boolean esInsumo) {
+		this.esInsumo = esInsumo;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(this==obj) {
+			return true;
+		}
+		if(!(obj instanceof ArticuloInsumo)) {
+			return false;
+		}
+		ArticuloInsumo a = (ArticuloInsumo) obj;
+		return this.id !=null && this.id.equals(a.getId());
+	}
+
+	@Override
+	public String toString() {
+		String obj = "Id: " + this.id +
+				"/ Denominacion: " + this.denominacion +
+				"/ PrecioCompra: " + this.precioCompra +
+				"/ PrecioVenta: " + this.precioVenta +
+				"/ StockActual: " + this.stockActual +
+				"/ StockMinimo: " + this.stockMinimo +
+				"/ UnidadDeMedida: " + this.unidadMedida +
+				"/ EsInsumo: " + this.esInsumo;
+		return obj;
+	}
+	
+	
+}
---
+++ BuenSabor/src/main/java/com/delivery/BuenSabor/articuloMfactDetalle/entity/ArticuloMfactDetalle.java
@@ -0,0 +1,66 @@
+package com.delivery.BuenSabor.articuloMfactDetalle.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "articulo_mfact_detalle")
+public class ArticuloMfactDetalle {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private double cantidad;
+	
+	@Column(name = "unidad_medida")
+	private String unidadMedida;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public double getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(double cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public String getUnidadMedida() {
+		return unidadMedida;
+	}
+
+	public void setUnidadMedida(String unidadMedida) {
+		this.unidadMedida = unidadMedida;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(this==obj) {
+			return true;
+		}
+		if(!(obj instanceof ArticuloMfactDetalle)) {
+			return false;
+		}
+		ArticuloMfactDetalle a = (ArticuloMfactDetalle) obj;
+		return this.id != null && this.id.equals(a.getId());
+	}
+
+	@Override
+	public String toString() {
+		String obj = "Id: " + this.id + "/ UnidadDeMedida: " + this.unidadMedida + "/ Cantidad: " + this.cantidad; 
+		return obj;
+	}
+	
+	
+}
