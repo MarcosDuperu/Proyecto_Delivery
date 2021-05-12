f219197ddbaa963f758ba1c4e4ba0ea1fafd294e
n3tting <n3tting@gmail.com>
2021-05-12 18:49:48 -0300

    Añadi jwt a perfil

--- FrontBuenSabor/src/app/components/perfil/perfil.component.ts
+++ FrontBuenSabor/src/app/components/perfil/perfil.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
@@ -22,5 +23,4 @@ export class PerfilComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
